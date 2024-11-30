1cff82f4b04c4f39b92a64aeecb711eb03a3518b
Holger Graef <holger.graef@gmail.com>
2024-11-30 23:23:11 +0100

    add libndef

---
+++ br-external/package/libndef/libndef.mk
@@ -0,0 +1,28 @@
+################################################################################
+#
+# libndef
+#
+################################################################################
+
+LIBNDEF_VERSION = 57ed905aab647d04137b39ba490df2b30c39579a
+LIBNDEF_SITE = https://gitlab.com/RPiAwesomeness/libndef.git
+LIBNDEF_SITE_METHOD = git
+LIBNDEF_INSTALL_STAGING = YES
+LIBNDEF_LICENSE = MIT
+LIBNDEF_LICENSE_FILES = LICENSE
+
+define LIBNDEF_BUILD_CMDS
+    $(MAKE) -C $(@D) all
+endef
+
+define LIBNDEF_INSTALL_STAGING_CMDS
+    mkdir -p $(STAGING_DIR)/usr/include/ndef-lite
+    $(INSTALL) -D -m 0644 $(@D)/include/ndef-lite/*.hpp $(STAGING_DIR)/usr/include/ndef-lite/
+    $(INSTALL) -D -m 0755 $(@D)/bin/libndef $(STAGING_DIR)/usr/lib/libndef.so
+endef
+
+define LIBNDEF_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/bin/libndef $(TARGET_DIR)/usr/lib/libndef.so
+endef
+
+$(eval $(generic-package))
