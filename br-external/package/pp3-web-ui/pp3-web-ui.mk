4ec29017bf0ce3eab7f4f68bd573819319ebc269
Holger Graef <holger.graef@gmail.com>
2024-12-30 22:27:38 +0100

    web ui: include build in buildroot

---
+++ br-external/package/pp3-web-ui/pp3-web-ui.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# pp3-web-ui
+#
+################################################################################
+
+PP3_WEB_UI_SITE = $(BR2_EXTERNAL_BR2RAUC_PATH)/package/pp3-web-ui
+PP3_WEB_UI_SITE_METHOD = local
+
+define PP3_WEB_UI_BUILD_CMDS
+    cd ${@D}/PlasticPlayer3 && /opt/flutter/bin/flutter build web
+endef
+
+define PP3_WEB_UI_INSTALL_TARGET_CMDS
+    rsync -avh --delete ${@D}/PlasticPlayer3/build/web/ $(TARGET_DIR)/usr/html
+endef
+
+$(eval $(generic-package))
