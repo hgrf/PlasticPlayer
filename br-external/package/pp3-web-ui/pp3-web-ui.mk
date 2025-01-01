9522b6163afd64b574b470b92214c754c1b393b0
Holger Graef <holger.graef@gmail.com>
2025-01-01 22:16:26 +0100

    web ui: make sure build/ and .dart_tool/ are clean

--- br-external/package/pp3-web-ui/pp3-web-ui.mk
+++ br-external/package/pp3-web-ui/pp3-web-ui.mk
@@ -8,6 +8,8 @@ PP3_WEB_UI_SITE = $(BR2_EXTERNAL_BR2RAUC_PATH)/package/pp3-web-ui
 PP3_WEB_UI_SITE_METHOD = local
 
 define PP3_WEB_UI_BUILD_CMDS
+    rm -rf ${@D}/PlasticPlayer3/build
+    rm -rf ${@D}/PlasticPlayer3/.dart_tool
     cd ${@D}/PlasticPlayer3 && /opt/flutter/bin/flutter build web
 endef
 
