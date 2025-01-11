68c0c2d74cd1349ae34a68582b9712fbdb8715f2
Holger Graef <holger.graef@gmail.com>
2025-01-11 13:46:16 +0100

    fix build on non-aarch64 systems

--- br-external/package/pp3-main/pp3-main.mk
+++ br-external/package/pp3-main/pp3-main.mk
@@ -10,7 +10,7 @@ PP3_MAIN_SITE_METHOD = local
 PP3_MAIN_DEPENDENCIES = libntag21x libndef ncurses
 
 define PP3_MAIN_BUILD_CMDS
-    $(CXX) -L$(TARGET_DIR)/usr/lib -I$(STAGING_DIR)/usr/include -I$(STAGING_DIR)/usr/include/glib-2.0 -I$(STAGING_DIR)/usr/lib/glib-2.0/include -o $(@D)/pp3-main \
+    $(TARGET_CXX) -L$(TARGET_DIR)/usr/lib -I$(STAGING_DIR)/usr/include -I$(STAGING_DIR)/usr/include/glib-2.0 -I$(STAGING_DIR)/usr/lib/glib-2.0/include -o $(@D)/pp3-main \
 		$(@D)/main.cpp $(@D)/librespot.c $(@D)/ui.c $(@D)/wifistatus.c \
 		-l:libntag21x.so.1.0.0 -lndef -lgpiod -lmenu -lncurses -lgio-2.0 -lglib-2.0 -lgobject-2.0
 endef
